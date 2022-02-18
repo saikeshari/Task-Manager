8a27bab89ff7cd96c98037aa1396183200e1d01b
Sai Keshari <saikeshari@gmail.com>
2022-02-18 22:20:53 +0530

    Deploy 6

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(cors());
 connectDB();
 
  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join("client/build")));
     app.get("*", (req,res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve("client", "build", "index.html"))
     );
   } else {
     app.get("/", function (req, res) {
